d829b7d24952b3e526d6a86c288700665dd7c4e9
u211-05 <u211-05@public.mpei.local>
2018-04-03 12:08:03 +0300

    add file

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ istream & operator >> (istream& in, Temperature& Temp);
 istream & operator >> (istream& in, Temperature& Temp){
     in >> Temp.temp;
     in >> Temp.scale;
-}
+};
 void test_temperature_input(){
     Temperature Temp;
     string inp = "10C";
@@ -32,19 +32,57 @@ void test_temperature_input(){
 };
 double convert(const Temperature& Temp, char scaleTo){
     double Kelvins;
-     switch (Temperature.scale){
+    if(scaleTo == Temp.scale){
+        return Temp.temp;
+    };
+     switch (Temp.scale){
+     case 'K':
+     case 'k':
+         Kelvins = Temp.temp;
+         break;
      case 'F':
-         Kelvins = ((9.0/5)*(Temperature.temp - 273) + 32);
+     case 'f':
+         Kelvins = (5*(Temp.temp - 32))/9 + 273.15;
          break;
      case 'C':
-         Kelvins = Temperature - 273.15;
+     case 'c':
+         Kelvins = Temp.temp - 273.15;
          break;
-     }
+     };
+    scaleTo = Temp.scale;
+    switch (scaleTo){
+    case 'K':
+    case 'k':
+        return Kelvins;
+        break;
+    case 'F':
+    case 'f':
+        return ((9.0/5)*(Kelvins - 273) + 32);
+        break;
+    case 'C':
+    case 'c':
+        return Kelvins - 273.15;
+        break;
+    };
+};
+bool operator < (const Temperature& lhs, const Temperature& rhs){
+    return convert(lhs, 'K') < convert(rhs, 'K');
+};
+Temperature operator - (const Temperature& lhs, Temperature& rhs){
+    Temperature retTemp;
+    retTemp.temp = convert(lhs, 'K') - convert(rhs, 'K');
+    retTemp .scale = 'K';
+    return retTemp;
+};
+Temperature operator / (const Temperature& lhs, Temperature& rhs){
+    Temperature retTemp;
+    retTemp.temp = convert(lhs, 'K') / convert(rhs, 'K');
+    retTemp .scale = 'K';
+    return retTemp;
 }
 int
 main() {
     test_temperature_input();
-    /*
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
@@ -113,6 +151,6 @@ main() {
         }
         cout << '\n';
     }
- */
+
     return 0;
 }
